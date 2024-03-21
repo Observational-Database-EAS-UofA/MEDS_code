bd123961cec6660d89b56789de4374f62a2d5bee
Caio Novais <caionovais@protonmail.com>
2024-03-21 17:29:24 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from read_MEDS_CAA_Baffin_1920_2021 import read_MEDS
+
+if __name__ == '__main__':
+    data_path = '/home/novaisc/workspace/obs_database/AW_CAA/CTD_DATA/MEDS_2021/original_data/test_data.csv'
+    read_MEDS(data_path)
